89e2124df8f29245f27d66281c42f0782be24758
hkss22 <103163674+hkss22@users.noreply.github.com>
2024-10-05 00:49:21 -0300

    Add files via upload

---
+++ Units/uConsts.pas
@@ -0,0 +1,13 @@
+unit uConsts;
+
+interface
+
+Const
+    USER_NAME = '99coders';
+    PASSWORD = '123456';
+    //BASE_URL = 'http://localhost:3000';
+    BASE_URL = 'http://autorack.proxy.rlwy.net:11540';
+
+implementation
+
+end.
